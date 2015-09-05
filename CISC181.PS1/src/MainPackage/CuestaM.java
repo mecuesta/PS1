f8d10042e537e36c34ad0c1a821a048eb78fd86c
mecuesta <mecuesta@udel.edu>
2015-09-05 17:52:05 -0400

    Created CuestaM.java and called that class in Main.java.

---
+++ CISC181.PS1/src/MainPackage/CuestaM.java
@@ -0,0 +1,11 @@
+package MainPackage;
+
+public class CuestaM {
+
+	public static void HelloWorld() {
+		//Display Hello World
+		System.out.println("Hello World");
+
+	}
+
+}
--- CISC181.PS1/src/MainPackage/Main.java
+++ CISC181.PS1/src/MainPackage/Main.java
@@ -3,8 +3,12 @@ package MainPackage;
 public class Main {
 
 	public static void main(String[] args) {
+		/*
+		 * My Github Account Name: <mecuesta>
+		 * 
+		 */
 			
-		GibbonsB.HelloWorld();
+		CuestaM.HelloWorld();
 	}
 
 }
